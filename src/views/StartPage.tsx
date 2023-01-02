fa995e26e07b33d03b4fa93082b0d62d4a564981
guakun01 <guakun01@gmail.com>
2023-01-02 11:09:08 +0800

    创建 mainlayout 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,12 +1,12 @@
-import {defineComponent, PropType, ref} from 'vue';
+import {defineComponent, ref} from 'vue';
 import {Button} from '../shared/Button';
 import {Center} from '../shared/Center';
 import {FloatButton} from '../shared/FloatButton';
 import {Icon} from '../shared/Icon';
-import {Navbar} from '../shared/Navbar';
 import s from './StartPage.module.scss';
 import {Overlay} from "../shared/Overlay";
 import {RouterLink} from "vue-router";
+import {MainLayout} from "../layouts/MainLayout";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -16,27 +16,28 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-        <Navbar>{
-          {
-            default: () => '使用火龙果',
-            icon: () => <Icon name="menu" class={s.nav_icon} onClick={onClickMenu}/>
-          }
-        }</Navbar>
-        <Center class={s.dragon_fruit_wrapper}><Icon name='dragon-fruit' class={s.dragon_fruit}/></Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/item/create">
-            <Button class={s.button}>记一笔</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/item/create">
-          <FloatButton iconName='add'/>
-        </RouterLink>
-        {refOverlayVisible.value
-          && <Overlay onClose={() => {
-            refOverlayVisible.value = false
-          }}/>}
-      </div>
+      <MainLayout>{
+        {
+          title: () => '使用火龙果',
+          icon: () => <Icon name="menu" class={s.nav_icon} onClick={onClickMenu}/>,
+          default: () => <>
+            <Center class={s.dragon_fruit_wrapper}><Icon name='dragon-fruit' class={s.dragon_fruit}/></Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/item/create">
+                <Button class={s.button}>记一笔</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to="/item/create">
+              <FloatButton iconName='add'/>
+            </RouterLink>
+            {refOverlayVisible.value
+              && <Overlay onClose={() => {
+                refOverlayVisible.value = false
+              }}/>
+            }
+          </>,
+        }
+      }</MainLayout>
     )
   }
 })
