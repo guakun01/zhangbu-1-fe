a0a60ee49b2f518f490afb4e82c5d9563c711c3a
guakun01 <guakun01@gmail.com>
2023-01-02 10:57:42 +0800

    创建 item & itemCreate 路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,23 +1,35 @@
-import { RouteRecordRaw } from "vue-router";
-import { First } from "../components/welcome/First";
-import { FirstAction } from "../components/welcome/FirstAction";
-import { Fourth } from "../components/welcome/Fourth";
-import { FourthAction } from "../components/welcome/FourthAction";
-import { Second } from "../components/welcome/Second";
-import { SecondAction } from "../components/welcome/SecondAction";
-import { Third } from "../components/welcome/Third";
-import { ThirdAction } from "../components/welcome/ThirdAction";
-import { StartPage } from "../views/StartPage";
-import { Welcome } from "../views/Welcome";
+import {RouteRecordRaw} from "vue-router";
+import {First} from "../components/welcome/First";
+import {FirstAction} from "../components/welcome/FirstAction";
+import {Fourth} from "../components/welcome/Fourth";
+import {FourthAction} from "../components/welcome/FourthAction";
+import {Second} from "../components/welcome/Second";
+import {SecondAction} from "../components/welcome/SecondAction";
+import {Third} from "../components/welcome/Third";
+import {ThirdAction} from "../components/welcome/ThirdAction";
+import {StartPage} from "../views/StartPage";
+import {Welcome} from "../views/Welcome";
+import {ItemPage} from "../views/ItemPage";
+import {ItemCreate} from "../components/item/ItemCreate";
+import {ItemList} from "../components/item/ItemList";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', redirect: '/welcome' },
-  { path: '/welcome', component: Welcome, children: [
-    { path: '', redirect: '/welcome/1' },
-    { path: '1', name: 'Welcome1', components: { main: First, footer: FirstAction } },
-    { path: '2', name: 'Welcome2', components: { main: Second, footer: SecondAction } },
-    { path: '3', name: 'Welcome3', components: { main: Third, footer: ThirdAction } },
-    { path: '4', name: 'Welcome4', components: { main: Fourth, footer: FourthAction } },
-  ]},
-  { path: '/start', component: StartPage },
+  {path: '/', redirect: '/welcome'},
+  {
+    path: '/welcome', component: Welcome, children: [
+      {path: '', redirect: '/welcome/1'},
+      {path: '1', name: 'Welcome1', components: {main: First, footer: FirstAction}},
+      {path: '2', name: 'Welcome2', components: {main: Second, footer: SecondAction}},
+      {path: '3', name: 'Welcome3', components: {main: Third, footer: ThirdAction}},
+      {path: '4', name: 'Welcome4', components: {main: Fourth, footer: FourthAction}},
+    ]
+  },
+  {path: '/start', component: StartPage},
+  {
+    path: '/item', component: ItemPage,
+    children: [
+      {path: '', component: ItemList},
+      {path: 'create', component: ItemCreate},
+    ]
+  },
 ]
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,35 +1,42 @@
 import {defineComponent, PropType, ref} from 'vue';
-import { Button } from '../shared/Button';
-import { Center } from '../shared/Center';
-import { FloatButton } from '../shared/FloatButton';
-import { Icon } from '../shared/Icon';
-import { Navbar } from '../shared/Navbar';
+import {Button} from '../shared/Button';
+import {Center} from '../shared/Center';
+import {FloatButton} from '../shared/FloatButton';
+import {Icon} from '../shared/Icon';
+import {Navbar} from '../shared/Navbar';
 import s from './StartPage.module.scss';
 import {Overlay} from "../shared/Overlay";
-export const StartPage = defineComponent({
-    setup: (props, context) => {
-        const refOverlayVisible = ref(true);
+import {RouterLink} from "vue-router";
 
-        const onClickMenu = () => {
-            console.log('x');
-            refOverlayVisible.value = !refOverlayVisible.value
-        }
-        return () => (
-            <div>
-                <Navbar>{
-                    {
-                        default: () => '使用火龙果',
-                        icon: () => <Icon name="menu" class={s.nav_icon} onClick={onClickMenu} />
-                    }
-                }</Navbar>
-                <Center class={s.dragon_fruit_wrapper}><Icon name='dragon-fruit' class={s.dragon_fruit} /></Center>
-                <div class={s.button_wrapper}>
-                    <Button class={s.button}>记一笔</Button>
-                </div>
-                <FloatButton iconName='add' />
-                {refOverlayVisible.value
-                    && <Overlay onClose={() => {refOverlayVisible.value = false}} />}
-            </div>
-        )
+export const StartPage = defineComponent({
+  setup: (props, context) => {
+    const refOverlayVisible = ref(false);
+
+    const onClickMenu = () => {
+      refOverlayVisible.value = !refOverlayVisible.value
     }
+    return () => (
+      <div>
+        <Navbar>{
+          {
+            default: () => '使用火龙果',
+            icon: () => <Icon name="menu" class={s.nav_icon} onClick={onClickMenu}/>
+          }
+        }</Navbar>
+        <Center class={s.dragon_fruit_wrapper}><Icon name='dragon-fruit' class={s.dragon_fruit}/></Center>
+        <div class={s.button_wrapper}>
+          <RouterLink to="/item/create">
+            <Button class={s.button}>记一笔</Button>
+          </RouterLink>
+        </div>
+        <RouterLink to="/item/create">
+          <FloatButton iconName='add'/>
+        </RouterLink>
+        {refOverlayVisible.value
+          && <Overlay onClose={() => {
+            refOverlayVisible.value = false
+          }}/>}
+      </div>
+    )
+  }
 })
