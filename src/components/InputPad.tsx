2296e64760f3d20e12894d88880986b36b385fdf
guakun01 <guakun01@gmail.com>
2023-01-02 15:38:06 +0800

    完成金额输入逻辑

--- src/components/InputPad.tsx
+++ src/components/InputPad.tsx
@@ -27,20 +27,92 @@ export const InputPad = defineComponent({
       hideDatePicker();
     }
 
+    const refAmount = ref('0');
+    const appendText = (n: number | string) => {
+      const nString = n.toString();
+      const dotIndex = refAmount.value.indexOf('.');
+      if (refAmount.value.length >= 13) return; // 金额不可以超过13位
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) return; // 小数点后保留两位
+      if (nString === '.') {
+        if (dotIndex >= 0) return // 小数点只可以输入一次
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 不可以输入 00
+          if (refAmount.value === '0') return;
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+
+      refAmount.value += nString;
+    }
+
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '提交', onClick: () => { } },
+      {
+        text: '1', onClick: () => {
+          appendText(1)
+        }
+      },
+      {
+        text: '2', onClick: () => {
+          appendText(2)
+        }
+      },
+      {
+        text: '3', onClick: () => {
+          appendText(3)
+        }
+      },
+      {
+        text: '4', onClick: () => {
+          appendText(4)
+        }
+      },
+      {
+        text: '5', onClick: () => {
+          appendText(5)
+        }
+      },
+      {
+        text: '6', onClick: () => {
+          appendText(6)
+        }
+      },
+      {
+        text: '7', onClick: () => {
+          appendText(7)
+        }
+      },
+      {
+        text: '8', onClick: () => {
+          appendText(8)
+        }
+      },
+      {
+        text: '9', onClick: () => {
+          appendText(9)
+        }
+      },
+      {
+        text: '.', onClick: () => {
+          appendText('.')
+        }
+      },
+      {
+        text: '0', onClick: () => {
+          appendText(0)
+        }
+      },
+      {
+        text: '清空', onClick: () => {
+          refAmount.value = '0'
+        }
+      },
+      {
+        text: '提交', onClick: () => {
+        }
+      },
     ]
 
     return () => <>
@@ -57,11 +129,9 @@ export const InputPad = defineComponent({
           </span>
           {/*<span><input type="date" value={time().format()} /></span>*/}
         </span>
-        <span class={s.amount}>123456789.01</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
-      <div class={s.buttons}>{
-        buttons.map(button => <button onClick={button.onClick}>{button.text}</button>)
-      }</div>
+      <div class={s.buttons}>{buttons.map(button => <button onClick={button.onClick}>{button.text}</button>)}</div>
     </>
   }
 })
