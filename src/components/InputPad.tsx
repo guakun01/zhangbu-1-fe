8410f50384f09d394f75eb8bb28418395bebee68
guakun01 <guakun01@gmail.com>
2023-01-02 14:19:04 +0800

    引入 vantui 完成日期组件

--- src/components/InputPad.tsx
+++ src/components/InputPad.tsx
@@ -1,7 +1,8 @@
-import {defineComponent, PropType} from 'vue';
+import {defineComponent, PropType, ref} from 'vue';
 import s from './InputPad.module.scss';
 import {Icon} from "../shared/Icon";
 import {time} from "../shared/time";
+import {DatetimePicker, Popup} from "vant";
 
 export const InputPad = defineComponent({
   props: {
@@ -10,30 +11,102 @@ export const InputPad = defineComponent({
     }
   },
   setup: (props, context) => {
+    const now = new Date();
+    const refDate = ref<Date>(now);
+    const refDatePickerVisible = ref(false);
+
+    const showDatePicker = () => {
+      refDatePickerVisible.value = true;
+    }
+    const hideDatePicker = () => {
+      refDatePickerVisible.value = false;
+    }
+
+    const setDate = (date: Date) => {
+      refDate.value = date;
+      hideDatePicker();
+    }
+
     const buttons = [
-      { text: '1', onClick: () => {} },
-      { text: '2', onClick: () => {} },
-      { text: '3', onClick: () => {} },
-      { text: '清空', onClick: () => {} },
-      { text: '4', onClick: () => {} },
-      { text: '5', onClick: () => {} },
-      { text: '6', onClick: () => {} },
-      { text: '+', onClick: () => {} },
-      { text: '7', onClick: () => {} },
-      { text: '8', onClick: () => {} },
-      { text: '9', onClick: () => {} },
-      { text: '-', onClick: () => {} },
-      { text: '.', onClick: () => {} },
-      { text: '0', onClick: () => {} },
-      { text: '删', onClick: () => {} },
-      { text: '提交', onClick: () => {} },
+      {
+        text: '1', onClick: () => {
+        }
+      },
+      {
+        text: '2', onClick: () => {
+        }
+      },
+      {
+        text: '3', onClick: () => {
+        }
+      },
+      {
+        text: '清空', onClick: () => {
+        }
+      },
+      {
+        text: '4', onClick: () => {
+        }
+      },
+      {
+        text: '5', onClick: () => {
+        }
+      },
+      {
+        text: '6', onClick: () => {
+        }
+      },
+      {
+        text: '+', onClick: () => {
+        }
+      },
+      {
+        text: '7', onClick: () => {
+        }
+      },
+      {
+        text: '8', onClick: () => {
+        }
+      },
+      {
+        text: '9', onClick: () => {
+        }
+      },
+      {
+        text: '-', onClick: () => {
+        }
+      },
+      {
+        text: '.', onClick: () => {
+        }
+      },
+      {
+        text: '0', onClick: () => {
+        }
+      },
+      {
+        text: '删', onClick: () => {
+        }
+      },
+      {
+        text: '提交', onClick: () => {
+        }
+      },
     ]
 
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon name="date" class={s.icon}/>
-          <span><input type="date" value={time().format()} /></span>
+          <span>
+            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
+              <DatetimePicker value={refDate.value} type="date" title="选择年月日"
+                              onCancel={hideDatePicker}
+                              onConfirm={setDate}/>
+            </Popup>
+          </span>
+          {/*<span><input type="date" value={time().format()} /></span>*/}
         </span>
         <span class={s.amount}>123456789.01</span>
       </div>
