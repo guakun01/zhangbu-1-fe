df7d7065d9b1af5ee5dc7e64b3a389f34432b160
guakun01 <guakun01@gmail.com>
2023-01-02 14:46:54 +0800

    使用 grid 布局重构 inputpad

--- src/components/InputPad.tsx
+++ src/components/InputPad.tsx
@@ -28,70 +28,19 @@ export const InputPad = defineComponent({
     }
 
     const buttons = [
-      {
-        text: '1', onClick: () => {
-        }
-      },
-      {
-        text: '2', onClick: () => {
-        }
-      },
-      {
-        text: '3', onClick: () => {
-        }
-      },
-      {
-        text: '清空', onClick: () => {
-        }
-      },
-      {
-        text: '4', onClick: () => {
-        }
-      },
-      {
-        text: '5', onClick: () => {
-        }
-      },
-      {
-        text: '6', onClick: () => {
-        }
-      },
-      {
-        text: '+', onClick: () => {
-        }
-      },
-      {
-        text: '7', onClick: () => {
-        }
-      },
-      {
-        text: '8', onClick: () => {
-        }
-      },
-      {
-        text: '9', onClick: () => {
-        }
-      },
-      {
-        text: '-', onClick: () => {
-        }
-      },
-      {
-        text: '.', onClick: () => {
-        }
-      },
-      {
-        text: '0', onClick: () => {
-        }
-      },
-      {
-        text: '删', onClick: () => {
-        }
-      },
-      {
-        text: '提交', onClick: () => {
-        }
-      },
+      { text: '1', onClick: () => { } },
+      { text: '2', onClick: () => { } },
+      { text: '3', onClick: () => { } },
+      { text: '4', onClick: () => { } },
+      { text: '5', onClick: () => { } },
+      { text: '6', onClick: () => { } },
+      { text: '7', onClick: () => { } },
+      { text: '8', onClick: () => { } },
+      { text: '9', onClick: () => { } },
+      { text: '.', onClick: () => { } },
+      { text: '0', onClick: () => { } },
+      { text: '清空', onClick: () => { } },
+      { text: '提交', onClick: () => { } },
     ]
 
     return () => <>
