27daef735777e9f2046d6cf445c3d0436d2d9885
guakun01 <guakun01@gmail.com>
2023-01-04 20:55:53 +0800

    完成记一笔的样式

--- src/components/items/ItemsCreate.tsx
+++ src/components/items/ItemsCreate.tsx
@@ -14,24 +14,98 @@ export const ItemsCreate = defineComponent({
   setup: (props, context) => {
     const refKind = ref('支出');
 
+    const refExpenseTags = ref([
+      {id: 1, name: '吃饭饭', sign: '￥', category: 'expense'},
+      {id: 2, name: '打车车', sign: '￥', category: 'expense'},
+      {id: 3, name: '聚餐餐', sign: '￥', category: 'expense'},
+      {id: 1, name: '吃饭饭', sign: '￥', category: 'expense'},
+      {id: 2, name: '打车车', sign: '￥', category: 'expense'},
+      {id: 3, name: '聚餐餐', sign: '￥', category: 'expense'},
+      {id: 1, name: '吃饭饭', sign: '￥', category: 'expense'},
+      {id: 2, name: '打车车', sign: '￥', category: 'expense'},
+      {id: 3, name: '聚餐餐', sign: '￥', category: 'expense'},
+      {id: 3, name: '聚餐餐', sign: '￥', category: 'expense'},
+    ]);
+
+    const refIncomeTags = ref([
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+      {id: 1, name: '发工资', sign: '￥', category: 'income'},
+      {id: 2, name: '发分红', sign: '￥', category: 'income'},
+      {id: 3, name: '中彩票', sign: '￥', category: 'income'},
+    ]);
+
     return () => (
       <MainLayout>{{
         title: () => '记一笔',
         icon: () => <Icon name="left" class={s.nav_icon}/>,
-        default: () => <>
+        default: () => <div class={s.wrapper}>
           {/*<Tabs selected={refKind.value} onUpdateSelected={name => refKind.value = name}>*/}
-          <Tabs v-model:selected={refKind.value}>
-            <Tab name="支出">
-              icon 列表1
+          <Tabs v-model:selected={refKind.value} class={s.tabs}>
+            <Tab name="支出" class={s.tags_wrapper}>
+              <div class={s.tag}>
+                <div class={s.sign}>
+                  <Icon name="add" class={s.createTag} />
+                </div>
+                <div class={s.name}>
+                  新增
+                </div>
+              </div>
+              {refExpenseTags.value.map(tag => {
+                return <div class={[s.tag, s.selected]}>
+                  <div class={s.sign}>
+                    {tag.sign}
+                  </div>
+                  <div class={s.name}>
+                    {tag.name}
+                  </div>
+                </div>
+              })}
             </Tab>
-            <Tab name="收入">
-              icon 列表2
+            <Tab name="收入" class={s.tags_wrapper}>
+              <div class={s.tag}>
+                <div class={s.sign}>
+                  <Icon name="add" class={s.createTag} />
+                </div>
+                <div class={s.name}>
+                  新增
+                </div>
+              </div>
+              {refIncomeTags.value.map(tag => {
+                return <div class={s.tag}>
+                  <div class={s.sign}>
+                    {tag.sign}
+                  </div>
+                  <div class={s.name}>
+                    {tag.name}
+                  </div>
+                </div>
+              })}
             </Tab>
           </Tabs>
           <div class={s.inputPad_wrapper}>
-            <InputPad />
+            <InputPad/>
           </div>
-        </>,
+        </div>,
       }}</MainLayout>
     )
   }
